b6c3e8de42b912461db10f536358b13d0a92db15
Hei Wiper <heiwiper@gmail.com>
2021-04-04 17:54:08 +0100

    Moved some code to tracker.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import glob
 import cv2
 import numpy as np
 from imutils.object_detection import non_max_suppression
-from random import randint
+from tracker import Tracker
 
 DIRECTORY_PATH = 'dataset/highway/input/'
 filenames = [img for img in glob.glob(DIRECTORY_PATH+"*.jpg")]
@@ -16,58 +16,9 @@ for frame in filenames:
 
 print("Finished reading images")
 
-file = open('trajectories.txt', 'w')
+HIST_THRESHOLD = 30
 
-count = 0
-ids = []
-tracking = []
-history = []
-colors = []
-historyThreshold = 30
-trajectories = []
-trackers = []
-class Trajectory:
-    def __init__(self):
-        self.points = []
-def addTracker(bbox, frame):
-    tracking.append(bbox)
-    # hls = (randint(0, 180), 127, 255)
-    # color = cv2.cvtColor(np.uint8([[hls]]), cv2.COLOR_HLS2BGR)[0][0]
-    # colors.append((int(color[0]),
-    #                int(color[1]),
-    #                int(color[2])))
-    colors.append((randint(0, 255),
-                   randint(0, 255),
-                   randint(0, 255)))
-    history.append(0)
-    global count
-    count += 1
-    ids.append(count)
-    newTrajectory = Trajectory()
-    trajectories.append(newTrajectory)
-
-    # newTracker = cv2.legacy.TrackerMedianFlow_create()  # Garbage
-    # newTracker = cv2.legacy.TrackerCSRT_create()  # good but doesn't report failure
-    newTracker = cv2.legacy.TrackerMOSSE_create()  # Very good
-    # newTracker = cv2.legacy.TrackerMIL_create()  # okay, doesn't report failure
-    # newTracker = cv2.TrackerGOTURN_create()
-    # newTracker = cv2.TrackerKCF_create()  # Very good
-    newTracker.init(frame, (bbox[0], bbox[1], bbox[2], bbox[3]))
-    trackers.append(newTracker)
-
-
-def removeTracker(i):
-    tracking.pop(i)
-    colors.pop(i)
-    file.write("Object {}\n".format(ids[i]))
-    for point in trajectories[i].points:
-        file.write("\t\tin{:06}\t".format(point[2]))
-        file.write("({}, {})\n".format(point[0], point[1]))
-    file.write("\n".format(ids[i]))
-    trajectories.pop(i)
-    ids.pop(i)
-    history.pop(i)
-    trackers.pop(i)
+tracker = Tracker()
 
 
 #Main Function
@@ -77,41 +28,38 @@ def main():
     for frameIndex, frame in enumerate(frames):
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         copy = frame.copy()
-        cars = car_cascade.detectMultiScale(gray,
+        objects = car_cascade.detectMultiScale(gray,
                                             scaleFactor=1.05,
                                             minNeighbors=13)
 
         # Replace the detections list with detected objects
         detections = []
-        for (x, y, w, h) in cars:
+        for (x, y, w, h) in objects:
             cv2.rectangle(copy, (x, y), (x+w, y+h), (0, 0, 255), 2)
             detections.append([x, y, w, h])
 
         # One or multiple objects detected
         if len(detections) != 0:
             # No object is being tracked
-            if len(tracking) == 0:
+            if len(tracker.bboxes) == 0:
                 # Add the detected objects to be tracked
                 for bbox in detections:
-                    addTracker(bbox, frame)
+                    tracker.addObject(bbox, frame)
 
-            elif len(tracking) < len(detections):
+            elif len(tracker.bboxes) < len(detections):
                 for index, bbox in enumerate(detections):
                     rects = np.array([[x, y, x+w, y+h]
-                                      for (x, y, w, h) in tracking+[bbox]])
+                                      for (x, y, w, h) in tracker.bboxes+[bbox]])
                     pick = non_max_suppression(rects, probs=None,
                                                overlapThresh=0.65)
 
                     # Object is no longer detected
-                    if pick.shape[0] == len(tracking) + 1:
-                        addTracker(bbox, frame)
-
-
-
+                    if pick.shape[0] == len(tracker.bboxes) + 1:
+                        tracker.addObject(bbox, frame)
 
             # There already is one or multiple objects being tracked
-            elif len(tracking) > 0:
-                for i, bbox in enumerate(tracking):
+            elif len(tracker.bboxes) > 0:
+                for i, bbox in enumerate(tracker.bboxes):
                     # Use non_max_suppression algorithm to check if the tracked
                     # object is still detected, this helps in removing objects
                     # which are no longer visible
@@ -123,46 +71,18 @@ def main():
                     # Object is no longer detected
                     if pick.shape[0] == len(detections) + 1:
                         # Object was detected in at least the 5 last frames
-                        if history[i] < historyThreshold:
-                            history[i] += 3
+                        if tracker.history[i] < HIST_THRESHOLD:
+                            tracker.history[i] += 3
                         # Object was not detected in 5 last frames, in this case
                         # we stop tracking the object
                         else:
-                            removeTracker(i)
-                            
+                            tracker.removeObject(i)
 
             # Track every object in the tracking list
-            for i, tracker in enumerate(trackers):
-                success, tracking[i] = tracker.update(frame)
-                if success is False:
-                    history[i] += 1
-                    if history[i] > historyThreshold:
-                        removeTracker(i)
-                else:
-                    x, y, w, h = tracking[i]
-                    cx = int((x + x + w) // 2)
-                    cy = int((y + y + h) // 2)
-                    trajectories[i].points.append((cx, cy, frameIndex))
-            for i, bbox in enumerate(tracking):
-                # Tracking success
-                p1 = (int(bbox[0]), int(bbox[1]))
-                p2 = (int(bbox[0] + bbox[2]), int(bbox[1] + bbox[3]))
-                cv2.rectangle(frame, p1, p2,
-                              (colors[i][0],
-                               colors[i][1],
-                               colors[i][2]),
-                              2, 1)
-                for point in trajectories[i].points:
-                    cv2.circle(frame, (point[0], point[1]), radius=1,
-                               color=(colors[i][0],
-                                      colors[i][1],
-                                      colors[i][2]),
-                               thickness=-1)
-                cv2.rectangle(copy, p1, p2,
-                              (colors[i][0],
-                               colors[i][1],
-                               colors[i][2]),
-                              2, 1)
+            tracker.update(frame, frameIndex, HIST_THRESHOLD)
+
+            # Draw bounding boxes and trajectories
+            tracker.draw(frame)
 
         cv2.imshow('Frames', frame)
         cv2.imshow('Copy', copy)
@@ -175,5 +95,5 @@ def main():
 if __name__ == "__main__":
     main()
 
-file.close() 
+tracker.file.close()
 print('Program Completed!')
---
+++ tracker.py
@@ -0,0 +1,110 @@
+import cv2
+from random import randint
+
+HISTORY_THRESHOLD = 30
+
+class Tracker:
+    def __init__(self):
+        # Main counter which used for objects IDs
+        self.count = 0
+
+        # Unique ID of each object
+        self.ids = []
+
+        # Bounding box of each tracked object
+        self.bboxes = []
+
+        # How many frames each object wasn't detected/tracked
+        self.history = []
+
+        # After how many frames the object should be discarded
+        self.historyThreshold = HISTORY_THRESHOLD
+
+        # Color of each object
+        self.colors = []
+
+        # List of each objects trajectory points
+        self.trajectories = []
+
+        # Tracker of each object
+        self.trackers = []
+
+        # File to save objects trajectories
+        self.file = open('trajectories.txt', 'w')
+
+    def addObject(self, bbox, frame):
+        self.bboxes.append(bbox)
+        # hls = (randint(0, 180), 127, 255)
+        # color = cv2.cvtColor(np.uint8([[hls]]), cv2.COLOR_HLS2BGR)[0][0]
+        # self.colors.append((int(color[0]),
+        #                int(color[1]),
+        #                int(color[2])))
+        self.colors.append((randint(0, 255),
+                            randint(0, 255),
+                            randint(0, 255)))
+        self.history.append(0)
+        self.count += 1
+        self.ids.append(self.count)
+        newTrajectory = Trajectory()
+        self.trajectories.append(newTrajectory)
+
+        # newTracker = cv2.legacy.TrackerMedianFlow_create()  # Garbage
+        # newTracker = cv2.legacy.TrackerCSRT_create()  # good but doesn't report failure
+        newTracker = cv2.legacy.TrackerMOSSE_create()  # Very good
+        # newTracker = cv2.legacy.TrackerMIL_create()  # okay, doesn't report failure
+        # newTracker = cv2.TrackerGOTURN_create()
+        # newTracker = cv2.TrackerKCF_create()  # Very good
+        newTracker.init(frame, (bbox[0], bbox[1], bbox[2], bbox[3]))
+        self.trackers.append(newTracker)
+
+    def removeObject(self, i):
+        self.bboxes.pop(i)
+        self.colors.pop(i)
+        self.file.write("Object {}\n".format(self.ids[i]))
+        for point in self.trajectories[i].points:
+            self.file.write("\t\tin{:06}\t".format(point[2]))
+            self.file.write("({}, {})\n".format(point[0], point[1]))
+        self.file.write("\n".format(self.ids[i]))
+        self.trajectories.pop(i)
+        self.ids.pop(i)
+        self.history.pop(i)
+        self.trackers.pop(i)
+
+    def update(self, frame, frameIndex, historyThreshold):
+        for i, tracker in enumerate(self.trackers):
+            success, self.bboxes[i] = tracker.update(frame)
+            if success is False:
+                self.history[i] += 1
+                if self.history[i] > historyThreshold:
+                    self.removeObject(i)
+            else:
+                x, y, w, h = self.bboxes[i]
+                cx = int((x + x + w) // 2)
+                cy = int((y + y + h) // 2)
+                self.trajectories[i].points.append((cx, cy, frameIndex))
+
+    def draw(self, frame):
+        for i, bbox in enumerate(self.bboxes):
+            p1 = (int(bbox[0]), int(bbox[1]))
+            p2 = (int(bbox[0] + bbox[2]), int(bbox[1] + bbox[3]))
+            cv2.rectangle(frame, p1, p2,
+                            (self.colors[i][0],
+                            self.colors[i][1],
+                            self.colors[i][2]),
+                            2, 1)
+            for point in self.trajectories[i].points:
+                cv2.circle(frame, (point[0], point[1]), radius=1,
+                            color=(self.colors[i][0],
+                                    self.colors[i][1],
+                                    self.colors[i][2]),
+                            thickness=-1)
+            # cv2.rectangle(copy, p1, p2,
+            #                 (colors[i][0],
+            #                 colors[i][1],
+            #                 colors[i][2]),
+            #                 2, 1)
+
+
+class Trajectory:
+    def __init__(self):
+        self.points = []
